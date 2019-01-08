69945805c12345af427cb00ac9e2cbc3e2511ebd
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-01-08 14:10:36 +0200

    Add files via upload
    Katsetus

--- harjutus.js
+++ harjutus.js
@@ -6,48 +6,49 @@ const lastResult = document.querySelector('.lastResult');
 const lowOrHigh = document.querySelector('.lowOrHigh');
 
 let guessCount = 1;
-let randomNumber = Math.floor(Math.random() * 100) +1; 
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 const checkGuess = () => {
     let userGuess = Number(guessField.value);
-    
-    if(guessCount === 1) {
+
+    if (guessCount === 1) {
         guesses.textContent = 'Eelnevad pakkumised : ';
-        
+
     }
-    //array
-//    var guessedNumbers = []
-//    guesseedNumbers.push(userGuess);
-//    if (userGuess.includes(userGuess)) {
-//        lastResult.textContent = 'Samat numbrit ei saa pakkuda'
-//    }
+    //    array
+    var guessedNumbers = [];
+    if (guessedNumbers.includes(userGuess)) {
+        return;
+        guessedNumbers.push(userGuess);
+    
+        lastResult.textContent = 'Samat numbrit ei saa pakkuda'
+    }
+    
     guesses.textContent += `${userGuess} `;
     //kui kasutaja pakub õigesti
     if (userGuess === randomNumber) {
         lastResult.textContent = 'Paljue õnne, võitsid!';
-        lastResult.style.backgroundColor='blue';
+        lastResult.style.backgroundColor = 'blue';
         lowOrHigh.textContent = '';
         setGameOver();
-    // kui 3 korda vastanud 
+        // kui 3 korda vastanud 
     } else if (guessCount === 3) {
-               lastResult.textContent = 'Mäng läbi';
+        lastResult.textContent = 'Mäng läbi';
         lowOrHigh.textContent = '';
         setGameOver();
     } else {
         lastResult.textContent = 'Vale vastus';
-        lastResult.style.backgroundColor='red';
-        
+        lastResult.style.backgroundColor = 'red';
+
         const lowOrHighText = 'Viimane pakkumine oli liiga ';
-        
-    //kui number liiga madal    
+        //kui number liiga madal    
         if (userGuess < randomNumber) {
             lowOrHigh.textContent = lowOrHighText + 'madal';
-    }
-            else if (userGuess > randomNumber) {
+        } else if (userGuess > randomNumber) {
             lowOrHigh.textContent = lowOrHighText + 'kõrge';
+        }
     }
-    }
-    
+
     guessCount++;
     guessField.value = '';
     guessField.focus();
@@ -64,25 +65,25 @@ function setGameOver() {
     resetButton.textContent = 'Alusta uut mängu';
     document.body.appendChild(resetButton);
     resetButton.addEventListener('click', resetGame);
-    location.reload();
+    //    location.reload();
 }
 
 function resetGame() {
-  guessCount = 1;
+    guessCount = 1;
 
-  const resetParas = document.querySelectorAll('.resultParas p');
-  for (let i = 0 ; i < resetParas.length ; i++) {
-    resetParas[i].textContent = '';
-  }
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for (let i = 0; i < resetParas.length; i++) {
+        resetParas[i].textContent = '';
+    }
 
-  resetButton.parentNode.removeChild(resetButton);
+    resetButton.parentNode.removeChild(resetButton);
 
-  guessField.disabled = false;
-  guessSubmit.disabled = false;
-  guessField.value = '';
-  guessField.focus();
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
 
-  lastResult.style.backgroundColor = 'white';
+    lastResult.style.backgroundColor = 'white';
 
-  randomNumber = Math.floor(Math.random() * 100) + 1;
+    randomNumber = Math.floor(Math.random() * 100) + 1;
 }
