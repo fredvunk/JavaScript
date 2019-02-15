c1dcf8174688b66676df0f91bc08953c6e07f5b4
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-02-15 11:33:22 +0200

    Lisatud loomade kirjeldused.

---
+++ Loomade varjupaik/script.js
@@ -0,0 +1,12 @@
+// src="https://unpkg.com/axios/dist/axios.min.js">
+//
+//     axios.get('/kassid.html')
+//         .then(function (response) {
+//             console.log(response);
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         })
+//         .then(function () {
+//
+//         });
