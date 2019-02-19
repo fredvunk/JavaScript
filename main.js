a07ebd088afe358a676872f0cfb879c598855c92
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-02-19 17:57:34 +0200

    Basic JS stuff

--- main.js
+++ main.js
@@ -1,4 +1,106 @@
-var number1 = '35';
-var number2 = '40';
+///////////// LOOPS/////////////////////
+// for(var i = 0; i < 10; i++) {
+//     console.log('ilove' +i);
+// }
+// var i = 0;
+// while(i < 10) {
+//     console.log(i);
+//     i++;
+// }
 
-alert(number1+number2);
+// var numbers = [33,54,76,34,2,6];
+// numbers.reverse();
+//
+// for(var i = 0;i < numbers.length;i++){
+//     console.log(numbers[i]);
+// }
+////////// LOOPS//////////////////////
+
+// CONDITION
+// var var1 = 3;
+// var var2 = 4;
+//
+// if(var1 == var2 && var1 == 3) {
+//     console.log('This is true');
+// } else {
+//         console.log('this is false');
+//     }
+
+// var fruit = 'grapes';
+//
+// switch(fruit){
+//     case "banana":
+//         alert('I love bananaas');
+//         break;
+//     case "apple":
+//         alert('I love apples');
+//         break;
+//     case "orange":
+//         alert('i i love oranges');
+//         break;
+//     default:
+//         alert('i love u');
+//         break;
+// }
+
+// OBJECTS ///////////////////
+///////// Object literal/////////////
+// var person = {
+//     firstName: 'Brad',
+//     lastName: 'trav',
+//     age: 34,
+//     children: ['briid', 'nike'],
+//     address: {
+//         street: '555 okey',
+//         city: 'boston',
+//         state: 'na'
+//     },
+//     fullName: function () {
+//         return this.firstName +" "+this.lastName;
+//
+//     }
+// }
+// console.log(person.fullName());
+////////object constructor ///////
+// function Fruit(name, color, shape) {
+//     this.name = name;
+//     this.color = color;
+//     this.shape = shape;
+//
+//     this.describe = function () {
+//         return 'A ' + this.name + ' is the color ' + this.color + ' and is the shape ' + this.shape;
+//     }
+// }
+//
+// // var apple = new Fruit('apple', 'red', 'round');
+//
+// var melon = new Fruit('melon', 'red', 'round');
+//
+// console.log(melon.describe());
+//
+// var users = [
+//     {
+//         name: ' John doe',
+//         age: 30
+//     },
+//     {
+//         name: ' Joh222n doe',
+//         age: 33
+//     },
+//     {
+//         name: ' Joh2233n doe',
+//         age: 39
+//     }
+// ];
+// console.log(users[2].name);
+
+// ///////////// EVENTS ///////////////////////
+// function showDate() {
+//    var time = document.getElementById('time');
+//    time.innerHTML = Date();
+// }
+// function clearDate() {
+//    var time = document.getElementById('time');
+//    time.innerHTML = '';
+// }
+////////////// F O R M S //////////////
