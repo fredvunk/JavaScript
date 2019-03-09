9c480221f446ae2c0418395cc18304803fb84ca9
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-03-09 16:02:21 +0200

    JS Basics Data types

--- JS_sandbox/main.js
+++ JS_sandbox/main.js
@@ -1,14 +1,32 @@
-// CONST
-const person = {
-    name: 'Fred',
-    age: 26
+// PRIMITIVE
+
+// // String
+// const name = 'fred';
+// // number
+// const age = 45;
+// // Boolean
+// const hasKids = true;
+// // Null
+// const car = null;
+// // Undefined
+// let test;
+// // Symbol
+// const sym = Symbol();
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof hasKids);
+// console.log(typeof car);
+// console.log(typeof test);
+// console.log(typeof sym);
+
+// Reference types - objects
+// array
+const hobbies = ['movies', 'music'];
+// object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
 }
-person.name = 'Krissu';
-person.age = 24;
-
-console.log(person);
-
-const numbers = [1,2,3];
-numbers.push(9);
-
-console.log(numbers);
+const today = new Date();
+console.log(today);
+console.log(typeof today);
