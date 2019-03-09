cb7cf41b1c7bdaf6898757056378bb733cb3a885
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-03-09 19:45:25 +0200

    JS Basic Object literals

--- JS_sandbox/main.js
+++ JS_sandbox/main.js
@@ -1,62 +1,38 @@
-// Arrays
-const numbers = [43,56,33,23,44,36,5];
-const numbers2 = new Array(22,45,33,76,54);
-const fruit = [
-    'apple',
-    'banana',
-    'orange',
-    'pear'
-];
-const mixed = [22, 'hello', true, undefined, null, {a:1, b:1}, new Date()];
+const person = {
+    firstName: 'Fred',
+    lastName: 'Vunk',
+    age: 30,
+    email: 'fred@hot.ee',
+    hobbies: ['music', 'gym'],
+    address: {
+        city: 'Tartu',
+        state: 'Eesti'
+    },
+    getBirthYear: function () {
+        return 2019 - this.age;
+    }
+}
 
 let val;
 
-//length
-val = numbers.length;
-// if is array
-val = Array.isArray(numbers);
-// single value
-val = numbers[3];
-// insert into
-numbers[2] = 100;
-// find index of value
-val = numbers.indexOf(36);
-// mutating arrays
-// // to end
-// numbers.push(250);
-// // to front
-// numbers.unshift(120);
-// //take off from end
-// numbers.pop();
-// // front
-// numbers.shift();
-// //Splice values
-// numbers.splice(1,1);
-// // rEVERSE
-// numbers.reverse();
-//concatenate array
-val = numbers.concat(numbers2);
-// sorting
-// val = fruit.sort();
-// val = numbers.sort();
-//
-// //compare function
-// val = numbers.sort(function(x, y){
-//     return x-y;
-// });
-// //reverse sort
-// val = numbers.sort(function(x, y){
-//     return y -x;
-// });
+val = person;
+// specific value
+val = person.firstName;
+val = person['lastName'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.address['city'];
+val = person.getBirthYear();
 
- //find
-function under50(num){
-    return num < 50;
-}
-
-val = numbers.find(under50);
-
-
-
-console.log(numbers);
 console.log(val);
+
+const people = [
+    {name: 'Joe', age: 22},
+    {name: 'joke', age: 23},
+    {name: 'jane', age: 24}
+];
+
+for(let i = 0; i < people.length; i++){
+    console.log(people[i].name);
+}
