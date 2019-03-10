11ff3e54d55978e9f1b9409defe4491bbada47ed
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-03-10 09:32:10 +0200

    JS Basic IF statements & Comparison Operators

--- JS_sandbox/main.js
+++ JS_sandbox/main.js
@@ -34,5 +34,59 @@ val = today.getTime();
 birthday.setMonth(2);
 birthday.setDate(12);
 
+//test if undefined
+//
+// if(typeof id !== undefined){
+//     console.log(`The ID is ${id}`);
+// } else {
+//     console.log('No ID');
+// }
 
-console.log(birthday);
+
+
+// if(id > 200){
+//     console.log('OK');
+// } else {
+//     console.log('NOT OK');
+// }
+
+// IF ELSE
+//
+// const color = 'rd';
+//
+// if(color === 'red'){
+//     console.log('Color is red');
+// } else if(color === 'blue'){
+//     console.log('Color is blue');
+// } else {
+//     console.log('Color is not red or blue');
+// }
+
+// LOGICAL OPERATORS
+
+const name = 'Fred';
+const age = 26;
+
+// AND &&
+
+if (age > 0 && age < 12) {
+    console.log(`${name} is a child`);
+} else if (age >= 13 && age <= 25) {
+    console.log(`${name} is a teenager`);
+} else {
+    console.log(`${name} is an adult`);
+}
+
+// OR ||
+
+if(age < 16 || age >65){
+    console.log(`${name} can not run in race`);
+} else {
+    console.log(`${name} is registred for the race`);
+}
+
+
+const id = 100;
+// TERNARY OPERATOR
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
