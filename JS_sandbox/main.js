d0511ccf1174528c6c3844e8b2d82abf0e15a80a
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-03-10 09:38:18 +0200

    JS Basic Switches

--- JS_sandbox/main.js
+++ JS_sandbox/main.js
@@ -90,3 +90,41 @@ const id = 100;
 // TERNARY OPERATOR
 console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
 
+switch(color){
+    case 'red':
+        console.log('Color is red');
+        break;
+    case 'blue':
+        console.log('Color is blue');
+        break;
+    default:
+        console.log('Color is not ');
+        break;
+}
+
+let day;
+
+switch(new Date().getDay()){
+    case 0:
+        day = 'sunday';
+        break;
+        case 1:
+        day = 'monday';
+        break;
+        case 2:
+        day = 'tuesday';
+        break;
+        case 3:
+        day = 'wednesday';
+        break;
+        case 4:
+        day = 'thursday';
+        break;
+        case 5:
+        day = 'friday';
+        break;
+        case 6:
+        day = 'saturday';
+        break;
+        }
+        console.log(`Today is ${day}`);
