42fa9fc15a2bfb6f01904d8b5d56f88bf3bd9ce1
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-03-11 20:57:23 +0200

    JS Basic Removing & Replacing elements

--- JS_sandbox/main.js
+++ JS_sandbox/main.js
@@ -1,35 +1,42 @@
+// REplace element
+
 // Create element
-const li = document.createElement('li');
+const newHeading = document.createElement('h2');
+// Add id
+newHeading.id = 'task-title';
+// New text node
+newHeading.appendChild(document.createTextNode('task list'));
+// Get the old heading
+const oldHeading = document.getElementById('task-title');
+// Parent
+const cardAction = document.querySelector('.card-action');
+// Replace
+cardAction.replaceChild(newHeading, oldHeading);
 
-// adding Class
-li.className = 'collection-item';
+// R E MOVE ELEMENT
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+// Remove list item
+lis[0].remove();
+// remove child element
+list.removeChild(lis[3]);
 
-//add id
-li.id = 'new-item';
+// CLASSES & ATTRIBUTES
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
 
-// Add attribute
-li.setAttribute('title','new item');
+let val;
+// Classes
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+val = link;
 
-// Create text node and append
-li.appendChild(document.createTextNode('Heeeey'));
+// Attributes
+val = link.getAttribute('href');
+// val = link.setAttribute('href', 'link') > takes
+// to link
+val = link.hasAttribute('title');
 
-// Create new link element
-const link = document.createElement('a');
-// Add classes
-link.className = 'delete-item secondary-content';
-
-// Add icon html
-link.innerHTML = '<i class="fa fa-remove"></i>';
-
-// Append link into li
-li.appendChild(link);
-
-// Cursor to pointer
-link.style.cursor = "crosshair";
-
-// Append li as child to ul
-document.querySelector('ul.collection').appendChild(li);
-
-
-
-console.log(li);
+console.log(val);
